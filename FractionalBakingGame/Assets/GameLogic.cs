057ea28cffcdcbfb95a572fcfa7c72e66236b1a0
Anup Bishnoi <pixelsallover@gmail.com>
2023-07-24 09:43:41 -0700

    Implement basic prototyping view with cake and knife

---
+++ FractionalBakingGame/Assets/GameLogic.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi! I'm a game logic script!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
