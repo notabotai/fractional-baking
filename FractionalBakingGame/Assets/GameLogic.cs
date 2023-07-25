c5e9a376dba8ca839da70c8f4e06b526db18677c
Anup Bishnoi <pixelsallover@gmail.com>
2023-07-25 10:58:55 -0700

    Basic line drawing by dragging mouse

--- FractionalBakingGame/Assets/GameLogic.cs
+++ FractionalBakingGame/Assets/GameLogic.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Hi! I'm a game logic script!");
-    }
-
+    public Vector3 dragStart;
+    public GameObject knife;
+    public LineRenderer sliceLine;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0;
+        knife.transform.position = worldPosition;
+
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Pressed left click at " + Input.mousePosition);
+            dragStart = worldPosition;
+        }
+
+        if (dragStart != Vector3.zero) {
+            // draw a line from dragStart to worldPosition
+            sliceLine.SetPositions(new Vector3[] { dragStart, worldPosition });
+        }
     }
 }
